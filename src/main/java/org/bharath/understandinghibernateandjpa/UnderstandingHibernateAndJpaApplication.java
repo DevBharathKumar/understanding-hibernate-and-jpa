85deec2656f6d96e3f697b4ef0d36624d37d364b
Bharath Kumar <bharathk494@gmail.com>
2020-05-26 14:06:52 +0530

    Initial Commit

---
+++ src/main/java/org/bharath/understandinghibernateandjpa/UnderstandingHibernateAndJpaApplication.java
@@ -0,0 +1,13 @@
+package org.bharath.understandinghibernateandjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnderstandingHibernateAndJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnderstandingHibernateAndJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/bharath/understandinghibernateandjpa/UnderstandingHibernateAndJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bharath.understandinghibernateandjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnderstandingHibernateAndJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
